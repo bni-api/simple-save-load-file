6d406d995a09292ebcd61a175d1eaa56e02b12c4
wildananugrah <wildananugrah@gmail.com>
2023-09-25 21:40:49 +0700

    init

---
+++ pages/api/read.js
@@ -0,0 +1,17 @@
+import fs from 'fs'
+
+const get = (req, res) => {
+    try {
+        res.json(JSON.parse(fs.readFileSync("save.json")))
+    } catch (error) {
+        console.log(error.message)
+    }
+    
+}
+
+const mapFunctions = {
+    GET: get
+}
+export default (req, res) => {
+    mapFunctions[req.method](req, res)
+}
---
+++ pages/api/save.js
@@ -0,0 +1,18 @@
+import fs from 'fs'
+
+const post = (req, res) => {
+    try {
+        fs.writeFileSync("save.json", JSON.stringify(req.body))
+        res.json({ message : "Customers data has been saved successfully." })
+    } catch (error) {
+        console.log(error.message)
+    }
+    
+}
+
+const mapFunctions = {
+    POST: post
+}
+export default (req, res) => {
+    mapFunctions[req.method](req, res)
+}
